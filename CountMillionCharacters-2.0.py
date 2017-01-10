479bbd4892e47f5ec2ba1a2d1459a4f04b68ffbc
Ayush Mishra <mishraayush945@gmail.com>
2017-01-10 23:15:15 +0530

    renaming the main function

--- CountMillionCharacters-2.0.py
+++ CountMillionCharacters-2.0.py
@@ -10,15 +10,15 @@ import pprint
 import collections
 
 
-def main():
+def rename():
 
     file_input = input('File Name: ')
 
-    with open(file_input, 'r') as info:
+    with open(file_input,'r') as info:
         count = collections.Counter(info.read().upper())
 
     value = pprint.pformat(count)
     print(value)
 
 if __name__ == "__main__":
-    main()
+    rename()
